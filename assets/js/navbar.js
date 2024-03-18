39fa09070e9947340b1a481223a021ea7add2b5d
Pedro Blaya Luz <blaya.luz@gmail.com>
2024-03-18 10:57:29 -0300

    tentativa resolução bug hover mobilie

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -7,29 +7,11 @@ function myFunction() {
     x.className = "topnav";
   }
 }
-
-function watchForHover() {
-  // lastTouchTime is used for ignoring emulated mousemove events
-  let lastTouchTime = 0
-
-  function enableHover() {
-    if (new Date() - lastTouchTime < 500) return
-    document.body.classList.add('hasHover')
-  }
-
-  function disableHover() {
-    document.body.classList.remove('hasHover')
-  }
-
-  function updateLastTouchTime() {
-    lastTouchTime = new Date()
-  }
-
-  document.addEventListener('touchstart', updateLastTouchTime, true)
-  document.addEventListener('touchstart', disableHover, true)
-  document.addEventListener('mousemove', enableHover, true)
-
-  enableHover()
-}
-
-watchForHover()
+$('.dropbtn').click(function () {
+  var isvisible = $(this).next('.dropdown-content').is(":visible")
+  $('.dropdown-content:visible').hide();
+  if (!isvisible)
+    {
+      $(this).next('.dropdown-content').toggle()
+    }
+})
